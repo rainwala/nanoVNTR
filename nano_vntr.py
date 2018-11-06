634bbb69ec2d110590d0bad18591397dbb20000f
Ali Raza Awan <ali.awan@gstt.nhs.uk>
2018-11-06 12:24:05 +0000

    added new methods to nano_vntr.py

--- nano_vntr.py
+++ nano_vntr.py
@@ -3,6 +3,8 @@ from Bio import SeqIO
 import difflib
 import re
 from fuzzywuzzy import fuzz
+from sklearn.cluster import KMeans
+import numpy as np
 
 class nanoVNTR:
 
@@ -13,6 +15,7 @@ class nanoVNTR:
         self.FLEN = len(LF) # flanking region length
         self.CUTOFF = CUTOFF # threshold for accepting a fuzzy match
         self.REP_CUTOFF = REP_CUTOFF # threshold for what percentage of the rep region needs to conform to rep multiples
+        self.REP_DICT = {} # stores read VNTR information
 
     ## gets the positions of fuzzy matches at least as good as the threshold of a query string within a larger string 
     ## provided that the repeat makes up at least cutoff proportion of the string
@@ -56,22 +59,43 @@ class nanoVNTR:
                     return {'LF': l_matches, 'RF': r_matches, 'VNTR': mid, 'REP_COUNT': num_reps}
         return None
 
-    ## given a fastq file of an alignment, returns a dict of read ID -> output from get_single_read_repeats
-    def get_all_read_repeats(self,fastq_file):
-        read_repeats = {}
-        for record in SeqIO.parse(fastq_file, "fastq"):
+    ## given a fasta or fastq file of an alignment, returns a dict of read ID -> output from get_single_read_repeats
+    def get_all_read_repeats(self,read_file,file_format):
+        self.REP_DICT = {} # to ensure idempotence
+        for record in SeqIO.parse(read_file,file_format):
             repeat = self.get_single_read_repeats(record)
             if repeat is not None:
-                read_repeats[record.id] = repeat
-        return read_repeats
+                self.REP_DICT[record.id] = repeat
     
-    ## prints all read statistics for reads in which VNTRs were found
+    ## prints all read summaries for reads in which VNTRs were found
+    def print_read_repeat_summaries(self):
+        outline = ""
+        for read_id in sorted(self.REP_DICT):
+            outline += read_id + "\t"
+            for key in ['LF','RF','VNTR','REP_COUNT']:
+                outline += key + ': ' + self.REP_DICT[key] + '\t'
+            print(outline.rstrip('\t'))
 
     ## converts the read_repeat dict to a list of repeat counts for allele finding
-
+    def convert_read_repeat_dict_to_repeat_count_list(self):
+        return [self.REP_DICT[read_id]['REP_COUNT'] for read_id in self.REP_DICT]
+        
     ## estimates the number of alleles in a list of repeat counts, based on variance
 
     ## given a list of repeat counts, an expected number of alleles (1 <= k <= 2) and a measure of average, 
     ## returns k VNTR counts
-
+    def get_VNTR_alleles(self,read_count_list,num_alleles,average_measure):
+        km = KMeans(n_clusters=num_alleles)
+        km.fit(np.array(read_count_list).reshape(-1,1))
+        if average_measure == 'centroid':
+            return [round(cc[0]) for cc in km.cluster_centers_]
+        else:
+            clusters = defaultdict(list)
+            for i in range(len(km.labels_)):
+                clusters[km_labels_[i]].append(read_count_list[i])
+            if average_measure == 'mean':
+                return [round(np.mean(clusters[l])) for l in clusters]
+            elif average_measure == 'median':
+                return [round(np.median(clusters[l])) for l in clusters]
+        return None
 
